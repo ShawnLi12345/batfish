1d5847cb654294459c0970291be1bbc86dbd4ce4
Shawn <liyixiao0608@gmail.com>
2026-04-03 21:16:52 -0700

    Finished question communityMatchUsage. This question outputs the number of community expressions each node uses.

--- projects/question/src/main/java/org/batfish/question/communitymatchusage/CommunityMatchUsageAnswerer.java
+++ projects/question/src/main/java/org/batfish/question/communitymatchusage/CommunityMatchUsageAnswerer.java
@@ -1,4 +1,86 @@
 package org.batfish.question.communitymatchusage;
 
-public class CommunityMatchUsageAnswerer {
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import java.util.Map;
+import org.batfish.common.Answerer;
+import org.batfish.common.NetworkSnapshot;
+import org.batfish.common.plugin.IBatfish;
+import org.batfish.datamodel.Configuration;
+import org.batfish.datamodel.answers.AnswerElement;
+import org.batfish.datamodel.answers.Schema;
+import org.batfish.datamodel.pojo.Node;
+import org.batfish.datamodel.questions.Question;
+import org.batfish.datamodel.routing_policy.communities.CommunityMatchExpr;
+import org.batfish.datamodel.table.ColumnMetadata;
+import org.batfish.datamodel.table.Row;
+import org.batfish.datamodel.table.TableAnswerElement;
+import org.batfish.datamodel.table.TableMetadata;
+import org.batfish.specifier.NodeSpecifier;
+import org.batfish.specifier.SpecifierContext;
+
+public class CommunityMatchUsageAnswerer extends Answerer {
+
+  public static final String COL_NODE = "Node";
+  public static final String COL_COMMUNITY_MATCH_COUNT = "Community_Match_Count";
+
+  public CommunityMatchUsageAnswerer(Question question, IBatfish batfish) {
+    super(question, batfish);
+  }
+
+  @Override
+  public AnswerElement answer(NetworkSnapshot snapshot) {
+    CommunityMatchUsageQuestion question = (CommunityMatchUsageQuestion) _question;
+    TableMetadata tableMetadata = createTableMetadata();
+    TableAnswerElement answer = new TableAnswerElement(tableMetadata);
+
+    List<Row> rows =
+        getAnswerRows(
+            _batfish.specifierContext(snapshot),
+            question.getNodeSpecifier(),
+            tableMetadata.toColumnMap());
+
+    answer.postProcessAnswer(question, rows);
+    return answer;
+  }
+
+  @VisibleForTesting
+  static List<Row> getAnswerRows(
+      SpecifierContext ctxt,
+      NodeSpecifier nodeSpecifier,
+      Map<String, ColumnMetadata> columnMap) {
+    ImmutableList.Builder<Row> rows = ImmutableList.builder();
+    int totalNodes = 0;
+    int matchingNodes = 0;
+
+    for (String nodeName : nodeSpecifier.resolve(ctxt)) {
+      totalNodes++;
+      Configuration config = ctxt.getConfigs().get(nodeName);
+      Map<String, CommunityMatchExpr> exprs = config.getCommunityMatchExprs();
+      if (!exprs.isEmpty()) {
+        matchingNodes++;
+        rows.add(
+            Row.builder(columnMap)
+                .put(COL_NODE, new Node(nodeName))
+                .put(COL_COMMUNITY_MATCH_COUNT, exprs.size())
+                .build());
+      }
+    }
+    return rows.build();
+  }
+
+  static TableMetadata createTableMetadata() {
+    List<ColumnMetadata> columns =
+        ImmutableList.of(
+            new ColumnMetadata(COL_NODE, Schema.NODE, "Node", true, false),
+            new ColumnMetadata(
+                COL_COMMUNITY_MATCH_COUNT,
+                Schema.INTEGER,
+                "Number of community match expressions",
+                false,
+                true));
+    return new TableMetadata(
+        columns, String.format("Node ${%s} has ${%s} community match expressions", COL_NODE, COL_COMMUNITY_MATCH_COUNT));
+  }
 }
--- projects/question/src/main/java/org/batfish/question/communitymatchusage/CommunityMatchUsageQuestion.java
+++ projects/question/src/main/java/org/batfish/question/communitymatchusage/CommunityMatchUsageQuestion.java
@@ -1,27 +1,32 @@
 package org.batfish.question.communitymatchusage;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.batfish.datamodel.questions.NodePropertySpecifier;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.batfish.datamodel.questions.Question;
-import org.batfish.question.nodeproperties.NodePropertiesQuestion;
 import org.batfish.specifier.AllNodesNodeSpecifier;
 import org.batfish.specifier.NodeSpecifier;
 import org.batfish.specifier.SpecifierFactories;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.Objects;
-
 public class CommunityMatchUsageQuestion extends Question {
   private static final String PROP_NODES = "nodes";
 
-  private @Nullable String _nodes;
-  private @Nonnull NodeSpecifier _nodeSpecifier;
+  private final @Nullable String _nodes;
+  private final @Nonnull NodeSpecifier _nodeSpecifier;
 
-  public CommunityMatchUsageQuestion(@Nullable String nodes){
+  @JsonCreator
+  private static CommunityMatchUsageQuestion create(
+      @JsonProperty(PROP_NODES) @Nullable String nodes) {
+    return new CommunityMatchUsageQuestion(nodes);
+  }
+
+  public CommunityMatchUsageQuestion(@Nullable String nodes) {
     _nodes = nodes;
-    _nodeSpecifier = SpecifierFactories.getNodeSpecifierOrDefault(nodes, AllNodesNodeSpecifier.INSTANCE);
+    _nodeSpecifier =
+        SpecifierFactories.getNodeSpecifierOrDefault(nodes, AllNodesNodeSpecifier.INSTANCE);
   }
 
 
