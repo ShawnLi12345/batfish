1992c964a95d25a10d26f98665b0cd5d9cb9eb45
Shawn Li <liyixiao0608@gmail.com>
2026-04-09 18:17:51 -0700

    Added question SubnetworkPolicyMetrics, which returns the count of Community Set Values, Set Metric, Match Community Values and Set Local Preference in the routing policies configured within the given subnet.

---
+++ projects/question/src/main/java/org/batfish/question/routepolicyproperties/SubnetworkPolicyMetricsQuestion.java
@@ -0,0 +1,64 @@
+package org.batfish.question.routepolicyproperties;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.batfish.datamodel.questions.Question;
+import org.batfish.specifier.AllNodesNodeSpecifier;
+import org.batfish.specifier.NodeSpecifier;
+import org.batfish.specifier.SpecifierFactories;
+
+public class SubnetworkPolicyMetricsQuestion extends Question {
+  private static final String PROP_NODES = "nodes";
+
+  private final @Nullable String _nodes;
+  private final @Nonnull NodeSpecifier _nodeSpecifier;
+
+  @JsonCreator
+  private static SubnetworkPolicyMetricsQuestion create(
+          @JsonProperty(PROP_NODES) @Nullable String nodes) {
+    return new SubnetworkPolicyMetricsQuestion(nodes);
+  }
+
+  public SubnetworkPolicyMetricsQuestion(@Nullable String nodes) {
+    _nodes = nodes;
+    _nodeSpecifier =
+            SpecifierFactories.getNodeSpecifierOrDefault(nodes, AllNodesNodeSpecifier.INSTANCE);
+  }
+
+  @Override
+  public boolean getDataPlane() {return false;}
+
+  @Override
+  public String getName() {
+    return "subnetworkPolicyMetrics";
+  }
+
+  @JsonProperty(PROP_NODES)
+  public @Nullable String getNodes() {
+    return _nodes;
+  }
+
+  @JsonIgnore
+  public @Nonnull NodeSpecifier getNodeSpecifier() {
+    return _nodeSpecifier;
+  }
+
+  @Override
+  public boolean equals(@Nullable Object o) {
+    if (!(o instanceof SubnetworkPolicyMetricsQuestion)) {
+      return false;
+    }
+    SubnetworkPolicyMetricsQuestion that = (SubnetworkPolicyMetricsQuestion) o;
+    return Objects.equals(_nodes, that._nodes)
+            && Objects.equals(_nodeSpecifier, that._nodeSpecifier);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(_nodes, _nodeSpecifier);
+  }
+}
